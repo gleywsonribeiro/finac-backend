b09038131e1f76dd1a2c64e86bd4fb0843319610
Gleywson Ribeiro <gleywson@gmail.com>
2023-09-09 19:56:20 -0300

    Iniando o projeto de Controle de Finanças

---
+++ src/test/java/com/gleywson/finacbackend/FinacBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gleywson.finacbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinacBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
